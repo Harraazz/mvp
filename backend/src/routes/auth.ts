f32f6507ac333dcaec2f922c61e5a32f3a9c8984
yusuf <yusufbbb444@gmail.com>
2026-04-13 00:42:14 +0700

    update api

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -84,4 +84,14 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+
+
+
+
+
 export default router;
